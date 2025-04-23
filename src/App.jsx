9305681153e4af6141664fcaffd42837467f45a7
VerifiedFans <troy@knoxconcepts.com>
2025-04-23 19:59:47 -0400

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import SeatPicker from "./SeatPicker";
+
+export default function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <SeatPicker />
+    </div>
+  );
+}
