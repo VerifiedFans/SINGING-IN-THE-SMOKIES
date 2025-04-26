42ac00231ec9ea1d1ead4ba380d9ec7d1dbb001c
VerifiedFans <troy@knoxconcepts.com>
2025-04-26 14:24:36 -0400

    Update supabaseClient.js

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "YOUR_SUPABASE_URL";         // ⬅️ paste from step 1
-const supabaseKey = "YOUR_ANON_PUBLIC_KEY";      // ⬅️ paste from step 1
+const supabaseUrl = "https://rkcupzdknbfhpizariud.supabase.co";         
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJrY3VwemRrbmJmaHBpemFyaXVkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDU1NDA3NTMsImV4cCI6MjA2MTExNjc1M30.aQa0ja_OQ9IDXjJ1k2szxgRpxcuteO6bbhI78ADheAw;      
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
