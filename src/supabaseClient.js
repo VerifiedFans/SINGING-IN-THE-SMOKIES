011afa6b608e7775695bf93af052fd0c061c933c
VerifiedFans <troy@knoxconcepts.com>
2025-04-26 15:25:57 -0400

    Update supabaseClient.js

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,4 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://rkcupzdknbfhpizariud.supabase.co";         
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJrY3VwemRrbmJmaHBpemFyaXVkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDU1NDA3NTMsImV4cCI6MjA2MTExNjc1M30.aQa0ja_OQ9IDXjJ1k2szxgRpxcuteO6bbhI78ADheAwexport const supabase = createClient(supabaseUrl, supabaseKey)";
+const supabaseUrl = "https://tutcofvvslgytyatijlu.supabase.co"; // 🔥 YOUR Supabase URL
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR1dGNvZnZ2c2xneXR5YXRpamx1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDUzNjgyNjksImV4cCI6MjA2MDk0NDI2OX0.7v9BwsO1oJpfSUCR4zmZTX4JeokgEIvSi5bnvckfzcc"; // 🔥 YOUR anon public API key
+
+export const supabase = createClient(supabaseUrl, supabaseKey);
