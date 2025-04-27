49fca3f23a67f16dc15dfb2b2cdf43516abffeb0
VerifiedFans <troy@knoxconcepts.com>
2025-04-26 23:29:43 -0400

    Update supabaseClient.js

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,1 @@
-import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://tutcofvvslgytyatijlu.supabase.co"; // 🔥 YOUR Supabase URL
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR1dGNvZnZ2c2xneXR5YXRpamx1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDUzNjgyNjksImV4cCI6MjA2MDk0NDI2OX0.7v9BwsO1oJpfSUCR4zmZTX4JeokgEIvSi5bnvckfzcc"; // 🔥 YOUR anon public API key
-
-export const supabase = createClient(supabaseUrl, supabaseKey);
